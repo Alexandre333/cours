2937bdf7b3f03f2d19702db84b6925a33cebc092
Alexandre Meyer <34301659+Alexandre333@users.noreply.github.com>
2021-03-06 13:14:22 +0200

    Add files via upload

---
+++ exercices_generaux.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Exercices</title>
+</head>
+<body>
+
+<?php
+	// Exercice 1 :
+	// Afficher un "Hello World" dans une balise H1
+
+	
+	// Exercice 2 :
+	// Modifier le code pour calculer la moyenne des notes
+	$note_maths = 15;
+	$note_francais = 12;
+	$note_histoire_geo = 9;
+	$moyenne = 0;
+	echo 'La moyenne est de '.$moyenne.' / 20.';
+	
+
+	// Exercice 3 :
+	/* Avec une boucle FOR, affichez 15 fois "J'adore le PHP"
+	avec le numéro de la ligne à coté*/
+
+
+	// Exercice 4 :
+	/* Imaginons : on a un budget de 1500€ et on souhaite 
+	acheter un produit à 1800€. Afficher si le budget le
+	permet en le vérifiant avec une condition IF */
+	$budget = 1500;
+	$produit = 1800;
+
+
+	// Exercice 5 :
+	// Afficher le 3ème élément de ce tableau
+	$fruits = array('papaye', 'mangue', 'litchi', 'kaki', 'avocat');
+
+	// Exercice 6 :
+	/* Compléter la fonction qui permet de convertir
+	des euros en dollars pour un prix donné */
+
+	$euros = 20;
+	
+	echo converDollar($euros);
+
+	function converDollar($prixEnEuros){
+
+	}
+
+?>
+
+	
+</body>
+</html>

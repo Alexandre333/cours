d2939c247a879f002c02dc7809e1c4209f4903e2
Alexandre Meyer <34301659+Alexandre333@users.noreply.github.com>
2021-03-23 12:17:10 +0200

    Rename wordpress/taxonomy.php to wordpress/wpcours/taxonomy.php

--- wordpress/taxonomy.php
+++
@@ -1,6 +0,0 @@
-<?php 
-/* 
-Template Name: taxonomy 
-*/ 
-?>
-<?php get_template_part('archive-recettes'); ?>
---
+++ wordpress/wpcours/taxonomy.php
@@ -0,0 +1,6 @@
+<?php 
+/* 
+Template Name: taxonomy 
+*/ 
+?>
+<?php get_template_part('archive-recettes'); ?>

1187bbe86fa21865ecaa371f19778feb214c5c42
Alexandre Meyer <34301659+Alexandre333@users.noreply.github.com>
2021-07-26 15:07:33 +0300

    Update py_api_json.py

--- dataviz/py_api_json.py
+++ dataviz/py_api_json.py
@@ -1,8 +1,15 @@
 #### Pour utiliser une API en ligne ####
 import requests
+import json
+
+# Fonction pour afficher le JSON en arbre
+def jprint(obj):
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
 
 # On exécute la requête API
 response = requests.get("http://api.openweathermap.org/data/2.5/weather?q=Paris,fr&units=metric&APPID=b0e08d79e61f6106a2b9f98d95df1e01")
+
 # On récupère la donnée JSON de la réponse de l'API
 data = response.json()
 
@@ -14,13 +21,7 @@ print("A Paris il fait : " + str(temperature) + "°C")
 vent = round(data['wind']['speed'] * 3.6, 1)
 print("Le vent souffle à " + str(vent) + " km/h")
 
-# Une fonction pour visualiser le JSON proprement
-def jprint(obj):
-    text = json.dumps(obj, sort_keys=True, indent=4)
-    print(text)
-
-jprint(response.json())
-
+"""
 ### Pour utiliser un fichier JSON en local ###
 import json
 
@@ -34,3 +35,4 @@ print(data)
 
 # On ferme le document
 f.close()
+"""
